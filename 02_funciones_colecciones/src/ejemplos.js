c73402bc06384d23a183af68c6c6d3c1d8356f50
Gabriel Rodríguez <franciscogabrielrodriguez7@gmail.com>
2026-03-09 15:26:19 -0500

    ejemplos map,filter,reduce

--- 02_funciones_colecciones/src/ejemplos.js
+++ 02_funciones_colecciones/src/ejemplos.js
@@ -54,15 +54,134 @@ console.log(totalFoodExpense); // Imprime 80
 
 // ejercicios
 // map
-// duplicar el precio a todos los productos
-const producto = [{nombre:"mesa",precio:5000},{nombre:"pera",precio:4300}]
-const precios = producto.map((precio) => ...producto,producto.precio: precio*2)
-console.log(precios)
-// aumentar la talla a un grupo de zapatillas cuando la persona crece
-const zapatillas =[{talla: 35},{talla:34},{talla:36},{talla:34}];
-const zapatillasgrandes = zapatillas.map((num) => num + 1);
+// 1 agregar apellido a un grupo de integrantes
+const apellido = " Rodriguez"
+const integrantes =["Gabriel","Andres","Mariana"]
+const familia = integrantes.map(function(integrantes){
+return{
+familia: integrantes + apellido
+}
+})
+console.log(integrantes)
+console.log(familia)
+// 2 ver caracteristica de un producto
+const producto = [
+{tipo:"fruta", nombre:"pera"}, 
+{tipo:"fruta", nombre:"manzana"},
+{tipo:"verdura", nombre:"zanahoria"} 
+]
+const tipo =producto.map(({tipo})=>tipo);
 
-// clasificar zapatos por tallas para una tienda
-const zapatos =[{marca:"nike",talla:35},{marca:"jordan",talla:34},{marca:"jordan",talla:36},{marca:"jordan",talla:34}];
-const tallazapatos = zapatos.filter((zapato) => zapato.talla === 34);
-console.log(tallazapatos);
+console.log(tipo)
+// 3 imprimir nota de alumnos aprobados
+const alumnos = [
+{nota:56, nombre:"Pepito"}, 
+{nota:56, nombre:"Marcos"},
+{nota:40, nombre:"Juan"} 
+]
+const aprobados = alumnos.map(function(alumnos){
+if(alumnos.nota>=50)
+  return `${alumnos.nombre} aprobó con: ${alumnos.nota}`;
+else if (alumnos.nota<50)
+  return `${alumnos.nombre} reprobó con: ${alumnos.nota}`;
+})
+console.log(aprobados) 
+// 4 claisificar mayor o menor de edad
+const edad = [
+{edad:26, nombre:"Pepito"}, 
+{edad:16, nombre:"Marcos"},
+{edad:19, nombre:"Juan"} 
+]
+const adultos= edad.map(function(edad){
+if (edad.edad>=18)
+  return `${edad.nombre}, eres mayor de edad`
+else if (edad.edad<18)
+  return `${edad.nombre}, eres menor de edad`
+})
+
+console.log(adultos)
+// 5 identificar tipos de datos
+
+const datos = [false,"a",4,true] 
+const tipoDato =datos.map(datos=>typeof(datos))
+console.table(tipoDato)
+
+// filter
+// 1 clasificar zapatos por tallas para una tienda
+const zapatos =[{marca:"nike",talla:35},
+{marca:"jordan",talla:34},
+{marca:"jordan",talla:36},
+{marca:"jordan",talla:34}
+];
+const tallaZapatos = zapatos.filter(zapato => zapato.talla === 34);
+console.table(tallaZapatos);
+
+// 2 clasificar ropa
+
+const ropa =[{tipo:"camisa",talla:"s"},
+{tipo:"zapatos",talla:"37"},
+{tipo:"pantalón",talla:"30"},
+{tipo:"camisa",talla:"m"}
+];
+const prendas = ropa.filter(ropa =>(ropa.tipo == "camisa" )||( ropa.tipo == "pantalón"))
+
+console.table(prendas)
+
+// 3 encontrar productos por tipo
+
+const productos =[{tipo:"fruta",nombre:"pera"},
+{tipo:"ropa",nombre:"camiseta"},
+{tipo:"ropa",nombre:"camisa"},
+{tipo:"fruta",nombre:"manzana"}
+];
+const tipoProducto = productos.filter(producto =>producto.tipo == "fruta" )
+
+console.table(tipoProducto)
+
+// 4 clasificar vendedores con 10 productos o más 
+
+const vendedores =[{np: 2,nombre:"pedro"},
+{np: 5,nombre:"eustaquio"},
+{np: 10,nombre:"esteban"},
+{np: 20,nombre:"pablo"}
+];
+const masProductos = vendedores.filter(vendedores =>vendedores.np >=10 )
+
+console.table(masProductos)
+
+// 5 clasificar usuarios
+
+const usuarios =[{tUsuario: "vendedor",nombre:"pedro"},
+{tUsuario: "cliente",nombre:"eustaquio"},
+{tUsuario: "empresa",nombre:"esteban"},
+{tUsuario: "cliente",nombre:"pablo"}
+];
+const clientes =usuarios.filter(({tUsuario})=> tUsuario == "cliente")
+
+console.table(clientes)
+
+// reduce 
+
+// 1 saber el número de productos que tiene cada vendedor
+
+const uVendedores = [{numProductos: 4,nombre:"pedro"},
+{numProductos: 7,nombre:"eustaquio"},
+{numProductos: 8,nombre:"esteban"},
+{numProductos: 4,nombre:"pablo"}
+];
+const numProduSistema =uVendedores.reduce((acumulador,uVendedores)=> acumulador + uVendedores.numProductos,0)
+
+console.log(numProduSistema)
+
+// 2 contar la cantidad de ingresos a una pagina
+
+const ingresos =["ingreso","cierre de sesión","ingreso","ingreso","cierre de sesión","ingreso"]
+
+const numIngresos=ingresos.reduce((contador,numIngresos)=> {
+  if (numIngresos!="ingreso")
+  return contador
+  if (numIngresos=="ingreso")
+  return contador +1
+},0)
+
+console.log(numIngresos)
